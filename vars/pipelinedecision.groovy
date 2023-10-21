9cb01124ba735615aa7d2572842680ce762a1a33
tanuja9346 <tanujach9346@gmail.com>
2023-10-21 19:56:45 +0530

    applications

--- vars/pipelinedecision.groovy
+++ vars/pipelinedecision.groovy
@@ -1,5 +1,19 @@
 #!groovy
 //inngroovy defining variable and function are same.
 def decidepipeline(Map configMap){
-echo "i need to take decision based on the map you sent "
+    application = configMap.get("application")
+    //here we are getting nodejsvm
+    switch(application) {
+        case 'nodeJSVM':
+         echo "application is Nodejs and vm based"
+            //nodeJSVMCI(configMap)    
+            break
+        case 'javavm' :
+              javaVMCI(configMap)
+              break
+        default: 
+          error: "unrecognized application"
+          break
+    }
+//echo "i need to take decision based on the map you sent "---1st practice line
 }
